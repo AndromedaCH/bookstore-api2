c2bb6c478cf97512041ccb1659d126e1224e9afd
caioh <caio.henrique.1998@hotmail.com>
2022-08-10 19:39:11 -0300

    classes finalizadas

--- src/main/java/com/caio/bookstore/domain/Categoria.java
+++ src/main/java/com/caio/bookstore/domain/Categoria.java
@@ -1,19 +1,30 @@
 package com.caio.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Categoria implements Serializable {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private static final long serialVersionUID = 1L;
 	private Integer id;
 	private String nome;
 	private String descricao;
-	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
-
+	
 	public Categoria() {
 		super();
-		// TODO Auto-generated constructor stub
+
 	}
 
 	public Categoria(Integer id, String nome, String descricao) {
